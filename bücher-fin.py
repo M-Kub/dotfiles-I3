f6b467ea4a2ea5da29e028eb59d6a08546bf1e15
stormy <mkubesch@gmail.com>
2018-11-06 20:31:00 +0100

    Added py book file

---
+++ bücher-fin.py
@@ -0,0 +1,33 @@
+import csv
+
+bücher = {}
+
+
+def buch_daten(bücher):
+    while True:
+        Autor = input("Bitte Namen des Autors eingeben! ")
+        Titel = input("Bitte den Titel des Buches eingeben! ")
+        bücher[Autor] = Titel
+        
+        weiter = input("Noch etwas hinzufügen? (y/n)")
+        if weiter == "y":
+            continue
+        elif weiter == "n":
+            print("Danke für die Eingabe:")
+            break
+        else:
+            print("Falsche Eingabe, dass Programm wird beendet.")
+            break
+
+    outputFile = open("books.csv", "a", newline="")
+    fieldnames = ['Autor', 'Titel']
+    outputWriter = csv.DictWriter(outputFile, fieldnames=fieldnames, delimiter="\t")
+    for key, val in bücher.items():
+        #outputWriter.writeheader()
+        outputWriter.writerow({"Autor" : key, "Titel" : val})
+
+        #f.write(f"{key}, {val},\n")
+        #w.writerow([key, val])
+        #print(f"Der Autor {key} und der Titel {val} wurden der Datei hinzugefügt.")
+
+buch_daten(bücher)
